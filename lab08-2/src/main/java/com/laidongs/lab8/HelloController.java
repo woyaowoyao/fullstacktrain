fd2cf3e63e4485442891b9285924928e38f4a2cb
dongshi <laidongs@cn.ibm.com>
2019-10-17 16:33:09 +0800

    push java files

---
+++ lab08-2/src/main/java/com/laidongs/lab8/HelloController.java
@@ -0,0 +1,17 @@
+package com.laidongs.lab8;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+//请求方式注解，value是指请求路径，method是请求方法
+@RequestMapping(value ="/hello",method = RequestMethod.GET)
+public String say(){
+return "hello Spring Boot";
+
+    }
+
+}
---
+++ lab08-2/src/main/java/com/laidongs/lab8/Lab81Application.java
@@ -0,0 +1,13 @@
+package com.laidongs.lab8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab81Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab81Application.class, args);
+	}
+
+}
