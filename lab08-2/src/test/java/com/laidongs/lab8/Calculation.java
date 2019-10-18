eae1c73331d4aeccea8ba38071e498bcf6da550c
dongshi <laidongs@cn.ibm.com>
2019-10-18 08:41:50 +0800

    add test

---
+++ lab08-2/src/test/java/com/laidongs/lab8/Calculation.java
@@ -0,0 +1,15 @@
+package com.laidongs.lab8;
+
+public class Calculation {
+
+	public static Object add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+	public static Object sub(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+
+}
--- lab08-2/src/test/java/com/laidongs/lab8/Lab81ApplicationTests.java
+++ lab08-2/src/test/java/com/laidongs/lab8/Lab81ApplicationTests.java
@@ -1,16 +1,39 @@
 package com.laidongs.lab8;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.junit.Assert;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Lab81ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
-}
+//	    private Calculation unit;
+	    @Before(value = "")
+	    public void before(){
+//	        unit = new Calculation();
+	    }
+	    
+		@Test
+	    public void testAdd(){
+	        Assert.assertEquals(Calculation.add(1, 2), 3);
+	        Assert.assertEquals(Calculation.add(100, 5), 105);
+	        Assert.assertEquals(Calculation.add(0, 0), 0);
+	        Assert.assertEquals(Calculation.add(Integer.MAX_VALUE, 0), Integer.MAX_VALUE);
+	        Assert.assertEquals(Calculation.add(Integer.MIN_VALUE, 0), Integer.MIN_VALUE);
+	    }	    
+	    @Test
+	    public void testSub(){
+	        Assert.assertEquals(Calculation.sub(1, 2), -1);
+	        Assert.assertEquals(Calculation.sub(100, 5), 95);
+	        Assert.assertEquals(Calculation.sub(0, 0), 0);
+	        Assert.assertEquals(Calculation.sub(-1, 0), -1);
+	        Assert.assertEquals(Calculation.sub(Integer.MAX_VALUE, 0), Integer.MAX_VALUE);
+	        Assert.assertEquals(Calculation.sub(Integer.MIN_VALUE, 0), Integer.MIN_VALUE);
+	    }
+	}
